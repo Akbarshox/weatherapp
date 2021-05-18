6b351183b98e765a66e0faddeb80c0d7be4a7e6a
Akbarshox <akbarshax@bk.ru>
2021-05-18 10:35:57 +0500

    initial Dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './dashboard.module.css';
+
+export default function Dashboard() {
+   return (
+      <div>
+         <p className={style.header}>ПРОГНОЗ ПОГОДЫ НА 5 ДНЕЙ</p>
+      </div>
+   )
+}
