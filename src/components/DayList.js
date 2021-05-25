7f863fcee3c5ba36f735e06fcb56eec294a07aaf
Akbarshox <akbarshax@bk.ru>
2021-05-25 16:56:20 +0500

    Expand with state

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useSelector} from "react-redux";
 import style from './dashboard.module.css';
-import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Collapse from '@material-ui/core/Collapse';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Accordion from "@material-ui/core/Accordion";
+import {ExpandLess} from "@material-ui/icons";
 
 export default function DayList() {
-   const data = useSelector(state => state.reducerWeather.data)
+   const data = useSelector(state => state.reducerWeather.data);
+   const [expanded, setExpanded] = useState(-1);
+
+   const handleExpand = (i) => {
+      setExpanded(expanded !== i ? i : -1);
+   }
 
    const Days = React.useMemo(() => {
       const grouped = data.list.filter((v, i, a) => a.findIndex(t => (t.dt_txt.substr(0, 10) === v.dt_txt.substr(0, 10))) === i).slice(0, 5);
@@ -34,41 +38,37 @@ export default function DayList() {
       const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
       return days[num];
    }
+
    const mainData = Days;
 
    return (
-      <div className={style.main}>
+      <div>
          {mainData.map((v, i) =>
-            <Accordion className={style.accordion}>
-               <AccordionSummary
-                  expandIcon={<ExpandMoreIcon/>}
-                  aria-controls="panel2a-content"
-                  id={i}
-               >
-                  <div className={style.container}>
-                     <div className={style.weekName}>
-                        <img src={getImg(v.weather[0].icon)} alt=""/>
-                        <p>{WeekDays(v.dt_txt.substr(0, 10))}</p>
-                     </div>
-                     <div className={style.degree}>
-                        {MaxMin(v.dt_txt.substr(0, 10), "temp_max")}°C/<span
-                        className={style.minTemp}>{MaxMin(v.dt_txt.substr(0, 10), "temp_min")}°C</span>
-                     </div>
-                     <div className={style.humid}>
-                        Влажность: {MaxMin(v.dt_txt.substr(0, 10), "humidity")}%
-                     </div>
+            <div className={style.main}>
+               <div className={style.container} onClick={() => handleExpand(i)}>
+                  <div className={style.weekName}>
+                     <img src={getImg(v.weather[0].icon)} alt=""/>
+                     <p>{WeekDays(v.dt_txt.substr(0, 10))}</p>
                   </div>
-               </AccordionSummary>
+                  <div className={style.degree}>
+                     {MaxMin(v.dt_txt.substr(0, 10), "temp_max")}°C/<span
+                     className={style.minTemp}>{MaxMin(v.dt_txt.substr(0, 10), "temp_min")}°C</span>
+                  </div>
+                  <div className={style.humid}>
+                     Влажность: {MaxMin(v.dt_txt.substr(0, 10), "humidity")}%
+                  </div>
+                  {/*<ExpandLess />*/}
+               </div>
                {getInfo(v.dt_txt.substr(0, 10)).map((el, idx) =>
-                  <AccordionDetails className={style.accordionDetails} key={idx}>
+                  <Collapse in={i === expanded} timeout="auto" className={style.accordionDetails} unmountOnExit>
                      <div className={style.hourly}>
                         <img src={getImg(el.weather[0].icon)} alt=""/>
                         <h3>{Math.round(el.main.temp)}°C</h3>
                         <p>{el.dt_txt.substr(11, 5)}</p>
                      </div>
-                  </AccordionDetails>
+                  </Collapse>
                )}
-            </Accordion>
+            </div>
          )}
       </div>
    )
